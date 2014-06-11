45852c315da7cced3a459cba91545fc16fa050cf
Shymon Shlafman <sshlafman@gmail.com>
2014-06-11 21:03:44 +0300

    Initial commit

---
+++ src/com/sshlafman/finances/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.sshlafman.finances;
+
+public class SavingsAccount {
+	private int balance = 0;
+
+	public void deposit(int amount) {
+		balance += amount;
+	}
+
+	public int balance() {
+		return balance;
+	}
+
+	public void withdraw(int amount) {		
+		balance -= amount;
+	}
+
+}
